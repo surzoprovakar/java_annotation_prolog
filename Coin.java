7beb8fdb1c39247ccf8c6e7f4a7caf96a6d0fcd1
Provakar Mondal <provakar@vt.edu>
2022-03-03 16:18:18 -0500

    Make the annotation expression more generic

--- Coin.java
+++ Coin.java
@@ -1,51 +1,42 @@
 import java.lang.annotation.Repeatable;
-@interface ProbabilityUniverse {}
 
-@interface ProbFair {
-  Type type();
-  int val();
+@interface OutcomeRange {
+	Class ann();	
+	int min();
+	int max();
 }
 
-@interface ProbFaces {
-  Faces face();
-  int val();
+@interface Prob {
+	String event() default "";	
+	int outcome();
+	int val();
 }
+
 @Repeatable(DependProbs.class)
 @interface DependProb {
-  Type type();
-  ProbFaces prob();
-}
-
-@ProbabilityUniverse
-enum Faces {
-  Head,
-  Tail
-}
-
-@ProbabilityUniverse
-enum Type {
-  Fair,
-  Biased
+	String event();	
+	Prob prob();
 }
 
 public class Coin {
- 
-  public void toss() {
-     double val = Math.random();
-     if (val >= thresholdHeads) {  
-        //example usage   
-     }
-  }
+	
+	public void toss() {
+		double val = Math.random();
+		if (val >= thresholdHeads) { 	
+			//example usage	
+		}
+	}
 
-  static {
-     thresholdHeads = probHeads();
-  }
- 
-  @ProbFair(type=Type.Fair, val=90)
-  @DependProb(type=Type.Fair, prob=@ProbFaces(face=Faces.Head, val=50))
-  @DependProb(type=Type.Biased, prob=@ProbFaces(face=Faces.Head, val=60))
-  public static native float probHeads();
- 
-  private final static float thresholdHeads;
+	static {
+		thresholdHeads = probHeads();
+	}
+
+	@OutcomeRange(ann=Prob.class, min=0, max=1)
+	@Prob(event="Fairness", outcome=1, val=90)
+	@DependProb(event="Fair", prob=@Prob(outcome=1, val=50))
+	@DependProb(event="Biased", prob=@Prob(outcome=1, val=60))
+	public static native float probHeads();
+	
+	private final static float thresholdHeads;
 
 }
